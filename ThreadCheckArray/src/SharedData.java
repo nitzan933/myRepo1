ed214fbb86b820d91b2fd1f2dcacc3f8368345c2
karin <karinsh97@gmail.com>
2021-10-27 11:34:41 +0300

    changed two files SharedData and ThreadCheckArray

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,11 +1,13 @@
+import java.util.List;
+
 public class SharedData 
 {
-	private int [] array;
+	private List<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
 	
-	public SharedData(int[] array, int b) {
+	public SharedData(List<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
@@ -21,7 +23,7 @@ public class SharedData
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	public List<Integer> getArray() 
 	{
 		return array;
 	}
